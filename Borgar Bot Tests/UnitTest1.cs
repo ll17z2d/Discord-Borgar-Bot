d6e282202c394f88a55a93873345575326503e7d
ll17z2d <ll17z2d@leeds.ac.uk>
2021-07-08 13:21:29 +0100

    Added test project

---
+++ Borgar Bot Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Borgar_Bot_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.IsFalse(false);
+        }
+    }
+}
